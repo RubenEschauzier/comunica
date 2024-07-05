9b96ca4d2e7f80663ea0b75791bbfbf88bff06b1
RubenEschauzier <ruben.eschauzier@gmail.com>
2024-07-05 11:50:46 +0200

    Implement topology aggregate tracker

--- packages/actor-context-preprocess-aggregate-statistic-traversed-topology/lib/ActorContextPreprocessAggregateStatisticTraversedTopology.ts
+++ packages/actor-context-preprocess-aggregate-statistic-traversed-topology/lib/ActorContextPreprocessAggregateStatisticTraversedTopology.ts
@@ -1,9 +1,12 @@
-import { ActorContextPreprocess, IActionContextPreprocess, IActorContextPreprocessOutput, IActorContextPreprocessArgs } from '@comunica/bus-context-preprocess';
+import type { StatisticsHolder } from '@comunica/actor-context-preprocess-set-defaults';
+import type { IActionContextPreprocess, IActorContextPreprocessOutput, IActorContextPreprocessArgs }
+  from '@comunica/bus-context-preprocess';
+import { ActorContextPreprocess } from '@comunica/bus-context-preprocess';
 import { KeysStatisticsTracker, KeysTrackableStatistics } from '@comunica/context-entries';
-import { IActorTest } from '@comunica/core';
+import type { IActorTest } from '@comunica/core';
 import type { IStatisticDereferencedLinks, IStatisticDiscoveredLinks } from '@comunica/types';
 import { AggregateStatisticTraversedTopology } from './AggregateStatisticTraversedTopology';
-import { StatisticsHolder } from '@comunica/actor-context-preprocess-set-defaults';
+
 /**
  * A comunica Aggregate Statistic Traversed Topology Context Preprocess Actor.
  */
@@ -12,23 +15,26 @@ export class ActorContextPreprocessAggregateStatisticTraversedTopology extends A
     super(args);
   }
 
-  public async test(action: IActionContextPreprocess): Promise<IActorTest> {
+  public async test(_action: IActionContextPreprocess): Promise<IActorTest> {
     return true;
   }
 
   public async run(action: IActionContextPreprocess): Promise<IActorContextPreprocessOutput> {
     const statisticsHolder: StatisticsHolder = action.context.get(KeysStatisticsTracker.statistics)!;
-    if (!statisticsHolder.get(KeysTrackableStatistics.discoveredLinks) || 
-        !statisticsHolder.get(KeysTrackableStatistics.dereferencedLinks))
-    {
-      throw new Error("Statistic aggregator did not find required statistics trackers in context");
+    if (!statisticsHolder.get(KeysTrackableStatistics.discoveredLinks) ||
+        !statisticsHolder.get(KeysTrackableStatistics.dereferencedLinks)) {
+      throw new Error('Statistic aggregator did not find required statistics trackers in context');
     }
 
     const discoverStatistic: IStatisticDiscoveredLinks = statisticsHolder.get(KeysTrackableStatistics.discoveredLinks)!;
-    const dereferenceStatistic: IStatisticDereferencedLinks = statisticsHolder.get(KeysTrackableStatistics.dereferencedLinks)!;
+    const dereferenceStatistic: IStatisticDereferencedLinks = statisticsHolder.get(
+      KeysTrackableStatistics.dereferencedLinks,
+    )!;
 
-    statisticsHolder.set(KeysTrackableStatistics.traversedTopology,
-      new AggregateStatisticTraversedTopology(discoverStatistic, dereferenceStatistic));
+    statisticsHolder.set(
+      KeysTrackableStatistics.traversedTopology,
+      new AggregateStatisticTraversedTopology(discoverStatistic, dereferenceStatistic),
+    );
     return { context: action.context };
   }
 }
--- packages/actor-context-preprocess-aggregate-statistic-traversed-topology/lib/AggregateStatisticTraversedTopology.ts
+++ packages/actor-context-preprocess-aggregate-statistic-traversed-topology/lib/AggregateStatisticTraversedTopology.ts
@@ -1,96 +1,108 @@
-// eslint-disable-next-line
+// eslint-disable-next-line import/no-nodejs-modules
 import { EventEmitter } from 'events';
 import type { ILink } from '@comunica/bus-rdf-resolve-hypermedia-links';
-import type { IDiscoverEventData, IStatistic, 
-    IStatisticDereferencedLinks, IStatisticDiscoveredLinks} from '@comunica/types';
+import type { IDiscoverEventData, IStatistic, IStatisticDereferencedLinks, IStatisticDiscoveredLinks }
+  from '@comunica/types';
+import { StatisticsHolder } from '@comunica/actor-context-preprocess-set-defaults';
+import { KeysTrackableStatistics } from '@comunica/context-entries';
+import { link } from 'fs';
 
 export class AggregateStatisticTraversedTopology implements IStatistic<ITopologyEventData> {
-    // Emitter that indicates when an update to the aggregate statistic happened
-    public statisticEvents: EventEmitter;
-    public toAggregate: Record<string, IStatistic<IDiscoverEventData | ILink>>;
+  // Emitter that indicates when an update to the aggregate statistic happened
+  public statisticEvents: EventEmitter;
+  public toAggregate: StatisticsHolder;
 
-    // Tracked data
-    public edgeList: Set<string>;
-    public metadata: Record<string, any>[];
-    public urlToIndex: Record<string, number>;
+  // Tracked data
+  public edgeList: Set<string>;
+  public metadata: Record<string, Record<string, any>>;
+  public urlToIndex: Record<string, number>;
 
+  public constructor(
+    discoverStatistic: IStatisticDiscoveredLinks,
+    dereferenceStatistic: IStatisticDereferencedLinks,
+  ) {
+    this.toAggregate = new StatisticsHolder();
+    this.toAggregate.set(KeysTrackableStatistics.discoveredLinks, discoverStatistic);
+    this.toAggregate.set(KeysTrackableStatistics.dereferencedLinks, dereferenceStatistic);
 
-    public constructor(
-        discoverStatistic: IStatisticDiscoveredLinks,
-        dereferenceStatistic: IStatisticDereferencedLinks
-    ){
-        this.toAggregate = {
-            discover: discoverStatistic, 
-            dereference: dereferenceStatistic
-        };
+    this.attachListeners();
+    this.statisticEvents = new EventEmitter();
+  }
 
-        this.attachListeners();
-        this.statisticEvents = new EventEmitter();
-    }
+  public attachListeners(): boolean {
 
-    public attachListeners(): boolean {
-        (<IStatisticDiscoveredLinks> this.toAggregate.discover).
-        addListener(this.processDiscoverEvent);
+    (<IStatisticDiscoveredLinks> this.toAggregate.get(KeysTrackableStatistics.discoveredLinks))
+      .addListener(this.processDiscoverEvent.bind(this));
 
-        (<IStatisticDereferencedLinks> this.toAggregate.dereference).
-        addListener(this.processDereferenceEvent);
+    (<IStatisticDereferencedLinks> this.toAggregate.get(KeysTrackableStatistics.dereferencedLinks))
+      .addListener(this.processDereferenceEvent.bind(this));
 
-        return true;
-    }
+    return true;
+  }
 
-    public addListener(cb: (arg0: ITopologyEventData) => void): void {
-        this.statisticEvents.addListener('data', cb);
-    }
+  public addListener(cb: (arg0: ITopologyEventData) => void): void {
+    this.statisticEvents.addListener('data', cb);
+  }
 
-    public processDiscoverEvent(data: IDiscoverEventData){
-        // TODO: Add edge to edgelist + overwrite existing metadata from discover events here
+  public processDiscoverEvent(data: IDiscoverEventData): void {
+    this.edgeList.add(JSON.stringify(data.edge));
 
-        const updatedTopology: ITopologyEventData = {
-            type: 'dereference',
-            edgeList: this.edgeList,
-            metadata: this.metadata
-        }
+    // Aggregate array of metadata entries for single node into one metadata entry with
+    // array values
+    const aggregatedMetadata: Record<any, any> = {}
+    data.metadataDiscoveredNode.map((metadata: Record<any, any>) => {
+        Object.keys(metadata).map( key => { aggregatedMetadata[key] = aggregatedMetadata[key] ? 
+            [...aggregatedMetadata[key], metadata[key]] : [metadata[key]];
+        })
+    });
+    
+    // Merge exising child metadata with incoming child metadata. On key overlap this will take values from aggregatedMetadata 
+    // as that is the most recently updated
+    this.metadata[data.edge[1]] = {...this.metadata[data.edge[1]], ...aggregatedMetadata}
 
-        // Emit new topology for other actors that want to use this info (e.g link prioritization)
-        this.emit(updatedTopology);
-    }
+    const updatedTopology: ITopologyEventData = {
+      type: 'dereference',
+      edgeList: this.edgeList,
+      metadata: this.metadata,
+    };
 
+    // Emit new topology for other actors that want to use this info (e.g link prioritization)
+    this.emit(updatedTopology);
+  }
 
-    public processDereferenceEvent(data: ILink){
-        // TODO: Add dereference metadata to existing metadata of link here
+  public processDereferenceEvent(data: ILink): void {
+    // When a dereference event happens, metadata is extended. 
+    this.metadata[data.url] = { ...this.metadata[data.url], ...data.metadata }
+    const updatedTopology: ITopologyEventData = {
+      type: 'dereference',
+      edgeList: this.edgeList,
+      metadata: this.metadata,
+    };
 
-        const updatedTopology: ITopologyEventData = {
-            type: 'dereference',
-            edgeList: this.edgeList,
-            metadata: this.metadata
-        }
+    // Emit new topology for other actors that want to use this info (e.g link prioritization)
+    this.emit(updatedTopology);
+  }
 
-        // Emit new topology for other actors that want to use this info (e.g link prioritization)
-        this.emit(updatedTopology);
-    }
-
-
-    public emit(data:  ITopologyEventData){
-        this.statisticEvents.emit('data', data);
-    }
+  public emit(data: ITopologyEventData): void {
+    this.statisticEvents.emit('data', data);
+  }
 }
-
 
 /**
  * Interface describing what data will be emitted when the topology is updated
  * Will emit all data so far instead of only the update that happened to the topology
  */
 export interface ITopologyEventData {
-    /**
-     * What type of update happened to the topology
-     */
-    type: 'dereference' | 'discover'
-    /**
-     * The edge list discovered during query execution
-     */
-    edgeList: Set<string>;
-    /**
-     * The metadata for each node
-     */
-    metadata: Record<string, any>[];
+  /**
+   * What type of update happened to the topology
+   */
+  type: 'dereference' | 'discover';
+  /**
+   * The edge list discovered during query execution
+   */
+  edgeList: Set<string>;
+  /**
+   * The metadata for each node
+   */
+  metadata: Record<string, Record<string, any>>;
 }
