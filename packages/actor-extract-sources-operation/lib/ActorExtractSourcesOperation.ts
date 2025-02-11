3c1d0992b87a6640c71e4ee0d58dbe19f825354c
RubenEschauzier <ruben.eschauzier@gmail.com>
2025-02-11 16:06:46 +0100

    Working merged join sampling and http caching

--- packages/actor-extract-sources-operation/lib/ActorExtractSourcesOperation.ts
+++ packages/actor-extract-sources-operation/lib/ActorExtractSourcesOperation.ts
@@ -24,23 +24,23 @@ export class ActorExtractSourcesOperation extends ActorExtractSources {
 
 
   public async run(action: IActionExtractSources): Promise<IActorExtractSourcesOutput> {
-    const sourcesOperations = action.operations.map(x => getSources(x));
-    const sources: Record<string, IQuerySource> = {};
-    sourcesOperations.map(x => x.map((sourceWrapper: IQuerySourceWrapper) => {
-      const sourceString = sourceWrapper.source.toString();
-      if (!sources[sourceString]) {
-        sources[sourceString] = sourceWrapper.source;
-      }
-    }));
+    // const sourcesOperations = action.operations.map(x => getSources(x));
+    // const sources: Record<string, IQuerySource> = {};
+    // sourcesOperations.map(x => x.map((sourceWrapper: IQuerySourceWrapper) => {
+    //   const sourceString = sourceWrapper.source.toString();
+    //   if (!sources[sourceString]) {
+    //     sources[sourceString] = sourceWrapper.source;
+    //   }
+    // }));
     
-    // Currently this only takes one source (a QuerySourceHypermedia) with possibly more source states
-    // (Will have to be more general / better in future)
-    const sourceToSample = <QuerySourceHypermedia><unknown>
-    (<QuerySourceSkolemized><unknown>sources[Object.keys(sources)[0]]).innerSource;
-    const sourcesInner = await Promise.all([...sourceToSample.sourcesState.keys()].map(async key => {
-      return (await sourceToSample.sourcesState.get(key)!).source
-    }));
+    // // Currently this only takes one source (a QuerySourceHypermedia) with possibly more source states
+    // // (Will have to be more general / better in future)
+    // const sourceToSample = <QuerySourceHypermedia><unknown>
+    // (<QuerySourceSkolemized><unknown>sources[Object.keys(sources)[0]]).innerSource;
+    // const sourcesInner = await Promise.all([...sourceToSample.sourcesState.keys()].map(async key => {
+    //   return (await sourceToSample.sourcesState.get(key)!).source
+    // }));
 
-    return {sources: sourcesInner };
+    return {sources: []};
   }
 }
