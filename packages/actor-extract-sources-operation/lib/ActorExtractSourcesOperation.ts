6706374cc2c38c3dd3205f7d7adc71fc24663f13
RubenEschauzier <ruben.eschauzier@gmail.com>
2025-02-17 11:32:41 +0100

    Add configurations sampling

--- packages/actor-extract-sources-operation/lib/ActorExtractSourcesOperation.ts
+++ packages/actor-extract-sources-operation/lib/ActorExtractSourcesOperation.ts
@@ -1,4 +1,3 @@
-import { QuerySourceSkolemized } from '@comunica/actor-context-preprocess-query-source-skolemize';
 import { 
   ActorExtractSources, 
   IActionExtractSources,
@@ -6,9 +5,8 @@ import {
    IActorExtractSourcesArgs 
   } from '@comunica/bus-extract-sources';
 import { IAction, IActorTest, passTestVoid, TestResult } from '@comunica/core';
-import { IQuerySource, IQuerySourceWrapper } from '@comunica/types';
-import { QuerySourceHypermedia } from '@comunica/actor-query-source-identify-hypermedia';
-import { getSources } from '@comunica/utils-query-operation';
+import { IQuerySource } from '@comunica/types';
+import { KeysCaches } from '@comunica/context-entries';
 
 /**
  * A comunica Operation Extract Sources Actor.
@@ -24,23 +22,11 @@ export class ActorExtractSourcesOperation extends ActorExtractSources {
 
 
   public async run(action: IActionExtractSources): Promise<IActorExtractSourcesOutput> {
-    // const sourcesOperations = action.operations.map(x => getSources(x));
-    // const sources: Record<string, IQuerySource> = {};
-    // sourcesOperations.map(x => x.map((sourceWrapper: IQuerySourceWrapper) => {
-    //   const sourceString = sourceWrapper.source.toString();
-    //   if (!sources[sourceString]) {
-    //     sources[sourceString] = sourceWrapper.source;
-    //   }
-    // }));
-    
-    // // Currently this only takes one source (a QuerySourceHypermedia) with possibly more source states
-    // // (Will have to be more general / better in future)
-    // const sourceToSample = <QuerySourceHypermedia><unknown>
-    // (<QuerySourceSkolemized><unknown>sources[Object.keys(sources)[0]]).innerSource;
-    // const sourcesInner = await Promise.all([...sourceToSample.sourcesState.keys()].map(async key => {
-    //   return (await sourceToSample.sourcesState.get(key)!).source
-    // }));
-
-    return {sources: [] };
+    const sources: IQuerySource[] = []
+    const withinQueryCache = action.context.getSafe(KeysCaches.withinQueryStoreCache);
+    for (const key of withinQueryCache.keys()){
+      sources.push( (await withinQueryCache.get(key)!).source);
+    }
+    return { sources }
   }
 }
