bbe7b9eae127386342cbb9a4944ec908c1b4058b
RubenEschauzier <ruben.eschauzier@gmail.com>
2025-07-02 10:16:52 +0200

    Working eddies with passing spec tests (for non-undef bindings)

--- packages/actor-rdf-join-inner-multi-stems/lib/EddieOperatorStream.ts
+++ packages/actor-rdf-join-inner-multi-stems/lib/EddieOperatorStream.ts
@@ -1,16 +1,16 @@
-import { Bindings } from '@comunica/utils-bindings-factory';
+import type { BindingsStream } from '@comunica/types';
+import type { Bindings } from '@comunica/utils-bindings-factory';
 
-import { AsyncIterator, BufferedIterator} from 'asynciterator';
 import type * as RDF from '@rdfjs/types';
-import { eddiesContextKeys, IEddieBindingsMetadata, ITimestampGenerator } from './EddieControllerStream';
-import { BindingsStream } from '@comunica/types';
-import { join } from 'node:path';
+import { BufferedIterator } from 'asynciterator';
+import type { IEddieBindingsMetadata, ITimestampGenerator } from './EddieControllerStream';
+import { eddiesContextKeys } from './EddieControllerStream';
 
 /**
  * We may want to implement AMJoin, which keeps track of a bitvector table to quickly determine join failures,
  * also look into how to route eddies tuples
  */
-export class EddieOperatorStream extends BufferedIterator<Bindings>{
+export class EddieOperatorStream extends BufferedIterator<Bindings> {
   /**
    * The variables that this operator can do a join on
    */
@@ -20,22 +20,22 @@ export class EddieOperatorStream extends BufferedIterator<Bindings>{
    */
   public variables: RDF.Variable[];
 
-  private sourceIterator: BindingsStream;
+  private readonly sourceIterator: BindingsStream;
 
-  private tripleMap: Map<number, Bindings[]> = new Map();
-  private timestampGenerator: ITimestampGenerator;
+  private readonly tripleMap: Map<number, Bindings[]> = new Map();
+  private readonly timestampGenerator: ITimestampGenerator;
 
-  private funHash: HashFunction;
-  private funJoin: JoinFunction;
+  private readonly funHash: HashFunction;
+  private readonly funJoin: JoinFunction;
 
-  private readBuffer: boolean = true; 
-  private bitLoc: number;
+  private readonly readBuffer = true;
+  private readonly bitLoc: number;
 
   private match: Bindings | null = null;
   private matches: Bindings[] = [];
-  private matchIdx: number = 0;
+  private matchIdx = 0;
 
-  private tempTotalReads: number = 0;
+  private tempTotalReads = 0;
 
   public constructor(
     sourceIterator: BindingsStream,
@@ -44,11 +44,11 @@ export class EddieOperatorStream extends BufferedIterator<Bindings>{
     funJoin: JoinFunction,
     bitLoc: number,
     variables: RDF.Variable[],
-    joinVariables: RDF.Variable[][]
-  ){
+    joinVariables: RDF.Variable[][],
+  ) {
     super();
 
-    this.variables = variables
+    this.variables = variables;
     this.joinVariables = joinVariables;
 
     this.funHash = funHash;
@@ -58,10 +58,10 @@ export class EddieOperatorStream extends BufferedIterator<Bindings>{
 
     if (this.sourceIterator.readable) {
       this.readable = true;
-    } 
+    }
 
     this.sourceIterator.on('readable', () => this.readable = true);
-    this.sourceIterator.on('error', (error) => this.destroy(error));
+    this.sourceIterator.on('error', error => this.destroy(error));
     this.sourceIterator.on('end', () => this.emit('endRead'));
 
     this.timestampGenerator = timestampGenerator;
@@ -69,8 +69,7 @@ export class EddieOperatorStream extends BufferedIterator<Bindings>{
     this.bitLoc = bitLoc;
   }
 
-  override _end ()
-  {
+  override _end() {
     super._end();
     this.sourceIterator.destroy();
     console.log(`EddieOperatorStream ended, total reads: ${this.tempTotalReads}`);
@@ -79,84 +78,79 @@ export class EddieOperatorStream extends BufferedIterator<Bindings>{
   push(item: IEddieJoinEntry) {
     // Push intermediate binding to buffer. We need to cast this to any
     // as the BufferedIterator does not know about the IEddieJoinEntry type.
-    this._push( <any> item);
+    this._push(<any> item);
   }
 
   override read() {
-    while(true){
-      if (this.ended){
+    while (true) {
+      if (this.ended) {
         return null;
       }
 
-      while (this.matchIdx < this.matches.length)
-      { 
-          let item = this.matches[this.matchIdx++];
-          const itemMetadata = item.getContextEntry(eddiesContextKeys.eddiesMetadata)!;
-          const matchMetadata = this.match!.getContextEntry(eddiesContextKeys.eddiesMetadata)!;
+      while (this.matchIdx < this.matches.length) {
+        const item = this.matches[this.matchIdx++];
+        const itemMetadata = item.getContextEntry(eddiesContextKeys.eddiesMetadata)!;
+        const matchMetadata = this.match!.getContextEntry(eddiesContextKeys.eddiesMetadata)!;
 
-          // If the timestamp of the interal index is newer (so larger)
-          // than the intermediate result it is not a valid join candidate
-          if (itemMetadata.timestamp > matchMetadata.timestamp) {
-            continue;
-          }
-          let result = this.funJoin(item, this.match!);
-          if (result !== null){
-            // Here we can just use this.match context entries as we simply are going to 
-            // set a bit entry to 1, this prevents extra work copying the object. Furthmore
-            // as we throw away the intermediate results, mutating the metadata is not a problem
-            // as we will never try to access it to get the previous metadata state.
-            const copy = {...matchMetadata}
-            copy.done |= 1 << this.bitLoc;
-            copy.order = [...copy.order!, this.bitLoc];
+        // If the timestamp of the interal index is newer (so larger)
+        // than the intermediate result it is not a valid join candidate
+        if (itemMetadata.timestamp > matchMetadata.timestamp) {
+          continue;
+        }
+        let result = this.funJoin(item, this.match!);
+        if (result !== null) {
+          // Here we can just use this.match context entries as we simply are going to
+          // set a bit entry to 1, this prevents extra work copying the object. Furthmore
+          // as we throw away the intermediate results, mutating the metadata is not a problem
+          // as we will never try to access it to get the previous metadata state.
+          const copy = { ...matchMetadata };
+          copy.done |= 1 << this.bitLoc;
+          copy.order = [ ...copy.order!, this.bitLoc ];
 
-            result = result.setContextEntry(eddiesContextKeys.eddiesMetadata, copy);
-            return result;      
-          }      
+          result = result.setContextEntry(eddiesContextKeys.eddiesMetadata, copy);
+          return result;
+        }
       }
 
-      let itemBuffer: IEddieJoinEntry | null  = null;
+      let itemBuffer: IEddieJoinEntry | null = null;
       let item = null;
       let joinVars: RDF.Variable[] | undefined;
-      // read from buffer first as these are intermediate results
+      // Read from buffer first as these are intermediate results
       itemBuffer = <IEddieJoinEntry | null> super.read();
-      if (itemBuffer !== null){
+      if (itemBuffer === null) {
+        item = this.sourceIterator.read();
+      } else {
         item = itemBuffer.item;
         joinVars = itemBuffer.joinVars;
       }
-      else{
-        item = this.sourceIterator.read();
-      } 
 
-      if (this.done || item === null)
-      {
+      if (this.done || item === null) {
         this.readable = false;
         return null;
       }
-
-      // TODO Allow joins on all variables that this stem represents!
 
       // We read from the sourceIterator. In this case we need to hash for each
       // possible join variable.
       item = <Bindings> item;
 
-      if (!joinVars){
-        const itemMetadata: IEddieBindingsMetadata =  {
+      if (!joinVars) {
+        const itemMetadata: IEddieBindingsMetadata = {
           done: 1 << this.bitLoc,
           timestamp: this.timestampGenerator.next(),
-          order: [this.bitLoc]
-        }
+          order: [ this.bitLoc ],
+        };
 
         item = item.setContextEntry(eddiesContextKeys.eddiesMetadata, itemMetadata);
 
         // For each variable we can join on, we hash the item and store it in the tripleMap.
         // This allows us to quickly find matches for any intermediate result and a given
         // join variable.
-        for (const joinVar of this.joinVariables){
-          const hash = this.funHash(item, joinVar); 
-          if (!this.tripleMap.has(hash)){
+        for (const joinVar of this.joinVariables) {
+          const hash = this.funHash(item, joinVar);
+          if (!this.tripleMap.has(hash)) {
             this.tripleMap.set(hash, []);
           }
-          let result = this.tripleMap.get(hash)!;
+          const result = this.tripleMap.get(hash)!;
           result.push(item);
         }
 
@@ -164,14 +158,13 @@ export class EddieOperatorStream extends BufferedIterator<Bindings>{
         return item;
       }
 
-      // We read from the buffer and got variables to join on. 
+      // We read from the buffer and got variables to join on.
       // So hash on the join variables.
-      else{
-        const hash = this.funHash(item, joinVars);
-        this.match = item;
-        this.matches = this.tripleMap.get(hash) || [];
-        this.matchIdx = 0;
-      }
+
+      const hash = this.funHash(item, joinVars);
+      this.match = item;
+      this.matches = this.tripleMap.get(hash) || [];
+      this.matchIdx = 0;
     }
   }
 }
--- packages/actor-rdf-join-inner-multi-stems/lib/EddieRouters.ts
+++ packages/actor-rdf-join-inner-multi-stems/lib/EddieRouters.ts
@@ -1,129 +1,165 @@
-import { Bindings } from '@comunica/utils-bindings-factory';
+import type { Bindings } from '@comunica/utils-bindings-factory';
 
-import { eddiesContextKeys } from "./EddieControllerStream";
 import type * as RDF from '@rdfjs/types';
+import { eddiesContextKeys } from './EddieControllerStream';
 
+export abstract class RouteBase implements IEddieRouter {
+  /**
+   * Route table mapping the done vector to a routing order
+   */
+  protected routeTable: Record<number, IEddieRoutingEntry[]>;
+  /**
+   * Connected components of join graph. Represented by their bitmask index
+   */
+  protected connectedComponents: number[][];
+  /**
+   * Number of triple patterns
+   */
+  protected n: number;
+  public constructor(n: number, connectedComponents: number[][]) {
+    this.routeTable = {};
+    this.n = n;
+  }
 
-export abstract class RouteBase implements IEddieRouter{
-    protected routeTable: Record<number, IEddieRoutingEntry[]>;
-    /**
-     * Number of triple patterns
-     */
-    protected n: number;
-    public constructor(n: number){
-        this.routeTable = {};
-        this.n = n;
-    }
+  public createRouteTable(variables: RDF.Variable[][]): Record<number, IEddieRoutingEntry[]> {
+    const n = variables.length;
+    const routeTable: Record<number, IEddieRoutingEntry[]> = {};
+    const variableValues = variables.map(vArr => vArr.map(x => x.value));
 
-    public createRouteTable(variables: RDF.Variable[][]): Record<number, IEddieRoutingEntry[]>{
-        const n = variables.length;
-        const routeTable: Record<number, IEddieRoutingEntry[]> = {};
-        const variableValues = variables.map(vArr => vArr.map(x => x.value));
+    const totalStates = 1 << n;
+    for (let state = 1; state < totalStates; state++) {
+      // Build doneVector array from bits of state
+      const doneIndexes = this.getSetBitIndexes(state);
+      // const connectedPatterns: number[] = [];
+      // const unconnectedPatterns: number[] = [];
 
-        const totalStates = 1 << n;
-        for (let state = 1; state < totalStates; state++) {
-            // Build doneVector array from bits of state
-            const doneIndexes = this.getSetBitIndexes(state);
+      // // Find for which connected components we have already processed a join.
+      // for (let i = 0; i<this.connectedComponents.length; i++){
+      //   if (this.connectedComponents[i].some(x => doneIndexes.includes(x))){
+      //     connectedPatterns.push(...this.connectedComponents[i]);
+      //   }
+      //   else{
+      //     unconnectedPatterns.push(...this.connectedComponents[i]);
+      //   }
+      // }
 
-            // TODO: Add this back when we fix support for multiple join variables
-            // // If all done, there is no next entry
-            if (doneIndexes.length === n) {
-                continue;
-            }
+      // // If all done, there is no next entry
+      if (doneIndexes.length === n) {
+        continue;
+      }
 
-            const doneVars = new Set(doneIndexes.flatMap(i => variableValues[i]));
+      const doneVars = new Set(doneIndexes.flatMap(i => variableValues[i]));
 
-            const possibleNext: IEddieRoutingEntry[] = [];
+      const possibleNext: IEddieRoutingEntry[] = [];
+      // Code to allow cartesian products in eddies but ensure they occur only when 
+      // ALL non-cartesian joins are made
 
-            for (let nextIdx = 0; nextIdx < n; nextIdx++) {
-                if ((state & (1 << nextIdx)) === 0){
-                    const varsNext = variables[nextIdx];
-                    const joinVars = varsNext.filter(v => doneVars.has(v.value));
-                    if (joinVars.length > 0) {
-                        possibleNext.push({ next: nextIdx, joinVars: joinVars });
-                    }         
-               }
-            }
-
-            routeTable[state] = possibleNext;
+      // for (const nextIdxConnected of connectedPatterns){
+      //   if ((state & (1 << nextIdxConnected)) === 0) {
+      //     const varsNext = variables[nextIdxConnected];
+      //     const joinVars = varsNext.filter(v => doneVars.has(v.value));
+      //     if(joinVars.length === 0){
+      //       throw new Error("Zero join vars in connected components something went wrong");
+      //     }
+      //     possibleNext.push({next: nextIdxConnected, joinVars});
+      //     break;
+      //   }
+      // }
+      // for (const nextIdxUnconnected of unconnectedPatterns){
+      //   if ((state & (1 << nextIdxUnconnected)) === 0) {
+      //     const varsNext = variables[nextIdxUnconnected];
+      //     const joinVars = varsNext.filter(v => doneVars.has(v.value));
+      //     possibleNext.push({next: nextIdxUnconnected, joinVars});
+      //   }
+      // }
+      
+      for (let nextIdx = 0; nextIdx < n; nextIdx++) {
+        if ((state & (1 << nextIdx)) === 0) {
+          const varsNext = variables[nextIdx];
+          const joinVars = varsNext.filter(v => doneVars.has(v.value));
+          if (joinVars.length > 0) {
+            possibleNext.push({ next: nextIdx, joinVars });
+          }
         }
-        this.routeTable = routeTable;
-        return routeTable
-    };
+      }
 
-    protected getSetBitIndexes(mask: number): number[] {
-        const indexes: number[] = [];
-        let position = 0;
-        while (mask !== 0) {
-            if ((mask & 1) === 1) {
-                indexes.push(position);
-            }
-            mask = mask >>> 1;  // unsigned right shift
-            position++;
-        }
-        return indexes;
+      routeTable[state] = possibleNext;
     }
+    this.routeTable = routeTable;
+    return routeTable;
+  };
 
-    protected getUnSetBitIndexes(mask: number): number[] {
-        const indexes: number[] = [];
-        for (let i = 0; i < this.n; i++) {
-            if ((mask & (1 << i)) === 0) {
-            indexes.push(i);
-            }
-        }
-        return indexes;    
+  protected getSetBitIndexes(mask: number): number[] {
+    const indexes: number[] = [];
+    let position = 0;
+    while (mask !== 0) {
+      if ((mask & 1) === 1) {
+        indexes.push(position);
+      }
+      mask >>>= 1; // Unsigned right shift
+      position++;
     }
-    abstract routeBinding(binding: Bindings): number | undefined;
-    abstract updateRouteTable(operatorMetadata: Record<string, any>[]): Record<number, IEddieRoutingEntry[]>;
+    return indexes;
+  }
+
+  protected getUnSetBitIndexes(mask: number): number[] {
+    const indexes: number[] = [];
+    for (let i = 0; i < this.n; i++) {
+      if ((mask & (1 << i)) === 0) {
+        indexes.push(i);
+      }
+    }
+    return indexes;
+  }
+
+  abstract routeBinding(binding: Bindings): number | undefined;
+  abstract updateRouteTable(operatorMetadata: Record<string, any>[]): Record<number, IEddieRoutingEntry[]>;
 }
 
-
-export class RouteRandom extends RouteBase{
-    public routeBinding(binding: Bindings): number | undefined{
-        const done = binding.getContextEntry(eddiesContextKeys.eddiesMetadata)!.done;
-        if (done === (1 << this.n) - 1){
-            return undefined
-        }
-        const indexesReadyEddies = this.getUnSetBitIndexes(done);
-        const nextEntry = this.sampleRandom(indexesReadyEddies);
-        return nextEntry;
+export class RouteRandom extends RouteBase {
+  public routeBinding(binding: Bindings): number | undefined {
+    const done = binding.getContextEntry(eddiesContextKeys.eddiesMetadata)!.done;
+    if (done === (1 << this.n) - 1) {
+      return undefined;
     }
+    const indexesReadyEddies = this.getUnSetBitIndexes(done);
+    const nextEntry = this.sampleRandom(indexesReadyEddies);
+    return nextEntry;
+  }
 
-    public updateRouteTable(operatorMetadata: Record<string, any>[]): Record<number, IEddieRoutingEntry[]>{
-        return this.routeTable
-    };
+  public updateRouteTable(operatorMetadata: Record<string, any>[]): Record<number, IEddieRoutingEntry[]> {
+    return this.routeTable;
+  };
 
-    private sampleRandom<T>(arr: T[]){
-        return arr[Math.floor(Math.random() * arr.length)];
-    }
+  private sampleRandom<T>(arr: T[]) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
 }
 
-
-export class RouteFixedMinimalIndex extends RouteBase{
-    public routeBinding(binding: Bindings): number | undefined {
-        const done = binding.getContextEntry(eddiesContextKeys.eddiesMetadata)!.done;
-        if (done === (1 << this.n) - 1){
-            return undefined
-        }
-        const indexesReadyEddies = this.getUnSetBitIndexes(done);
-
-        return Math.min(...indexesReadyEddies);
+export class RouteFixedMinimalIndex extends RouteBase {
+  public routeBinding(binding: Bindings): number | undefined {
+    const done = binding.getContextEntry(eddiesContextKeys.eddiesMetadata)!.done;
+    if (done === (1 << this.n) - 1) {
+      return undefined;
     }
-    public updateRouteTable(operatorMetadata: Record<string, any>[]): Record<number, IEddieRoutingEntry[]>{
-        return this.routeTable
-    };
+    const indexesReadyEddies = this.getUnSetBitIndexes(done);
 
+    return Math.min(...indexesReadyEddies);
+  }
+
+  public updateRouteTable(operatorMetadata: Record<string, any>[]): Record<number, IEddieRoutingEntry[]> {
+    return this.routeTable;
+  };
 }
-
 
 // Eddie routers should support batched routing with certain batchsize
-export interface IEddieRouter{
-    routeBinding: (binding: Bindings) => number | undefined;
-    createRouteTable: (variables: RDF.Variable[][]) => Record<number, IEddieRoutingEntry[]>;
-    updateRouteTable: (operatorMetadata: Record<string, any>[]) => Record<number, IEddieRoutingEntry[]>;
+export interface IEddieRouter {
+  routeBinding: (binding: Bindings) => number | undefined;
+  createRouteTable: (variables: RDF.Variable[][]) => Record<number, IEddieRoutingEntry[]>;
+  updateRouteTable: (operatorMetadata: Record<string, any>[]) => Record<number, IEddieRoutingEntry[]>;
 }
 
 export interface IEddieRoutingEntry {
-    next: number;
-    joinVars: RDF.Variable[];
+  next: number;
+  joinVars: RDF.Variable[];
 }
